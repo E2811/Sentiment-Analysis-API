79c9c96b379fe696e94f38e6fe36660a21176926
Elisa <elisammontalvo28@gmail.com>
2020-02-23 22:54:29 +0100

    subimos los primeros archivos a falta de revision

---
+++ api.py
@@ -0,0 +1,54 @@
+#!/usr/local/bin/python3
+from flask import Flask, request
+import users as U
+import chat as C
+
+
+app = Flask(__name__)
+
+@app.route("/user/create/<username>", methods=['GET'])
+def create_user(username):
+    return U.createUser(username) 
+
+
+@app.route("/chat/create")
+def create_chat():
+    users_id = request.args.get('users_id')
+    def comma_separated_params_to_list(param):
+        param =param[1:-1]
+        result = []
+        for val in param.split(','):
+            if val:
+                result.append(val)
+        return result
+    users_id = comma_separated_params_to_list(users_id)
+    return C.chat__list_create(users_id)
+
+@app.route("/chat/<chat_id>/adduser")
+def add_user_chat(chat_id):
+    user_id = request.args.get('user_id')
+    return C.add_user(chat_id,user_id)
+
+@app.route("/chat/<chat_id>/addmessage")
+def addMessage(chat_id):
+    user_id = int(request.args.get('user_id'))
+    message = request.get_json()
+    return C.add_message(chat_id, user_id, message)
+
+@app.route("/chat/<chat_id>/list")
+def chatList(chat_id):
+    return C.chat_list(chat_id)
+
+@app.route("/user/<user_id>/recommend")
+def recommendUser(user_id):
+    return U.userRecommend(user_id)
+
+@app.route("/user/<user_id>/recommendation")
+def recommend_user(user_id):
+    return U.userRecommendWord(user_id)
+
+@app.route("/chat/<chat_id>/sentiment")
+def sentimentScore(chat_id):
+    return C.sentiment_chat(chat_id)
+
+app.run("0.0.0.0", 5000, debug=True)
---
+++ users.py
@@ -0,0 +1,36 @@
+from pymongo import MongoClient, InsertOne
+from bson.json_util import dumps
+from errorHandler import error_handler
+import recommender as R
+
+
+client = MongoClient("mongodb://localhost:27017/")
+db =client['chatgroup']
+users = db['users']
+
+@error_handler
+def createUser(username):
+    ''' Create users in mongodb and return the user_id '''
+    query = list(users.find({'username':username}, {'_id':1}))
+    if query:
+        raise ValueError(f'username alredy exists {query}')
+    user_id = uniqueID()
+    users.insert_one({'user_id':user_id, 'username':username})
+    return {'user_id': str(user_id), 'username':username}
+
+
+def userRecommend(user_id):
+    ''' Recommend friends to this user based on chat sentiments '''
+    return R.recommend_sentiment(int(user_id))
+
+def userRecommendWord(user_id):
+    ''' Recommend friends to this user based on chat similar words used '''
+    return R.recommend_similar(int(user_id))
+
+
+def uniqueID():
+    ''' Create new ID ''' 
+    user_id =[e['user_id'] for e in list(users.find({},{'_id':0, 'user_id':1}))]
+    if not user_id:
+        return 1
+    return max(user_id) +1
